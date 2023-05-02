6d2296c8f7e90dc1e7fb3fa18f73c22c8512378c
Akzhol <cheder911@gmail.com>
2023-05-02 20:18:24 +0600

    1

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -16,13 +16,17 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
-        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");            System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");            System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+        System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
+
 
 
     }
